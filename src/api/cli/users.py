574a9b7385ffed8ac80c2690a5604692e74d88c7
Beki Imanji <imanaliev@akhter.studio>
2024-02-21 01:34:53 +0600

    creating a user through the client's cli

--- src/api/cli/users.py
+++ src/api/cli/users.py
@@ -1,13 +1,25 @@
 import click
 from faker import Faker
 
+from src.core.database import sync_session_impl
+from src.infra.repositories.users import UserRepository
+from src.services.users import UserCreateService
+
 fake = Faker()
 
 
 @click.command()
 @click.option('--username', prompt='Your username', required=True)
 @click.option('--password', prompt='Your password', required=True)
-@click.option('--firstname', required=False, default=fake.first_name)
-@click.option('--lastname', required=False, default=fake.last_name)
-def insert_user(username: str, password: str, firstname: str, lastname: str) -> None:
-    ...
+@click.option('--first_name', required=False, default=fake.first_name)
+@click.option('--last_name', required=False, default=fake.last_name)
+def insert_user(username: str, password: str, first_name: str, last_name: str) -> None:
+    session = sync_session_impl()
+    repo = UserRepository(session=session)
+    service = UserCreateService(repo=repo, session=session)
+    service.execute(
+        username=username,
+        password=password,
+        first_name=first_name,
+        last_name=last_name,
+    )
