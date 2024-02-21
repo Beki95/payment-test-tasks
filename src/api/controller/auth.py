753ac0ce363e458c85e5f1195382ea566f58ffbc
Beki Imanji <imanaliev@akhter.studio>
2024-02-22 01:12:50 +0600

    using bruteforce logic for authorization

--- src/api/controller/auth.py
+++ src/api/controller/auth.py
@@ -2,24 +2,33 @@ from fastapi import (
     APIRouter,
     Depends,
 )
+from starlette.requests import Request
 from starlette.responses import Response
 
-from src.interfaces.repositories.users import IUsersRepository
+from src.interfaces.repositories.alchemy.users import IUsersRepository
+from src.interfaces.repositories.redis._ip import IBlockedIPRepository
 from src.services.auth.request_dto import UserLoginRequest
 from src.services.auth.service import (
     AuthLogoutService,
     AuthorizationService,
 )
-from src.services.common import get_current_user
+from src.services.common import (
+    LockoutManager,
+    get_current_user,
+)
 
 auth = APIRouter(prefix='/auth', tags=['auth'])
 
 
 @auth.post('/login')
 async def login(
-    dto: UserLoginRequest, repo: IUsersRepository = Depends(),
+    request: Request,
+    dto: UserLoginRequest,
+    blocked_cache_repo: IBlockedIPRepository = Depends(),
+    repo: IUsersRepository = Depends(),
 ):
-    service = AuthorizationService(repo=repo)
+    lockout_manager = LockoutManager(request=request, blocked_cache_repo=blocked_cache_repo)
+    service = AuthorizationService(repo=repo, lockout_manager=lockout_manager)
     return await service.authenticate(dto=dto, response=Response())
 
 
